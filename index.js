8fd251e7403575e391882dba12b9cc1fd0a968a1
thamanbharti <117168416+thamanbharti@users.noreply.github.com>
2023-06-08 01:36:04 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,50 +1,85 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import Login from './login';
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import Productex from './productex';
-import ADDTOCART from './addtocart';
-import Register from './register';
+import express from "express";
+import mongoose from "mongoose";
+import cors from 'cors';
+// line 6:helps to import module as react in package.json
+
+const app=express();
+ app.use(express.json())
+ app.use(express.urlencoded())
+ app.use(cors())
+
+ ///for connection of database
+ mongoose.connect('mongodb://localhost:27017/mywebd', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => {
+    console.log('Connected to the database');
+    // Additional code or operations after successful connection
+  })
+  .catch((error) => {
+    console.error('Error connecting to the database:', error);
+  });
 
 
-const router = createBrowserRouter([
-  {
-    path: "/login",
-    element: <Login/>,
-  },
-  {
-    path: "/",
-    element: <App/>,
-  },
-  {
-    path: "/productex",
-    element: <Productex/>,
-  },
-  {
-    path: "/addtocart",
-    element: <ADDTOCART/>,
-  },
-  {
-    path: "/register",
-    element:< Register/>
-  }
-]);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    
-    <RouterProvider router={router}/>
-  </React.StrictMode>
-);
+  //user schema -variable for storing data or model in these format data will stored
+  const userSchema=mongoose.Schema({
+    name:String,
+    email:String,
+    passwrd:String
+  })
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  const User=new mongoose.model("user",userSchema)
+ //Routes
+
+ //post-helps to send data to destination
+ app.post("/login",(req,res)=>{
+   const {email,passwrd}=req.body
+   User.findOne({email:email},(err,user)=>{
+    if(user){
+         if(passwrd===user.passwrd)
+         {
+            res.send({message:"login successfull",user:user})
+         }
+         else{
+            res.send({message:'didnt cmatch password'})
+         }
+    }
+    else{
+        res.send({message:"user not registerd"})
+    }
+   })
+ })
+
+ app.post("/register",(req,res)=>{
+   const {name,email,passwrd}=req.body  
+   ///findOne -function given by mongod to find if there preexist data with initiall entered data or
+   // it can be assumed like to check if user is registerd already
+   User.findOne({email:email},(err,user)=>{
+    if(user)
+    {
+       
+        res.send({message:'user already registerd'})
+    }
+    else{   
+         const user=new User({     ///helps to create new object for user with data=name,email,passwrd
+        name,
+        email,
+        passwrd
+       })
+       user.save(err=>{  //save function help to save the data user that is created above
+        if(err){
+            res.send(err)
+        }
+        else {
+            res.send({message:"successfully registered"})
+        }
+       })}
+   })
+
+ })
+
+ app.listen(9002,()=>{
+    console.log("BE started at port 9002")
+ })
